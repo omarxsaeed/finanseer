318615926bbdba380368d593a19487b2b69cf88a
omarxsaeed <omar.ahmed.saeed.1999@gmail.com>
2023-09-03 22:30:13 +0300

    add: mui config and theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { useMemo } from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 function App() {
-  return <div className="app" />;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>App</h1>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
