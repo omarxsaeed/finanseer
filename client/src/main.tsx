bf6c12586cf6bdfbd97d5fd4f634b5f9b1530263
omarxsaeed <omar.ahmed.saeed.1999@gmail.com>
2023-09-05 21:36:46 +0300

    add: redux toolkit setup and configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "@/index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./state/api";
+import App from "./App";
+
+const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
+
+export default store;
