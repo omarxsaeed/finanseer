2d18d2966babb1482b023920af5868b707cfbb06
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-09-07 03:35:38 +0300

    chore: Update database loader to drop and seed database

--- server/src/loaders/database.ts
+++ server/src/loaders/database.ts
@@ -1,14 +1,27 @@
 import mongoose from "mongoose";
 import config from "../config";
+import { kpis } from "../data";
+import KPI from "../api/kpis/kpis.model";
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(config.MONGO_URL);
+    const db = await mongoose.connect(config.MONGO_URL);
     console.log("Database connected");
+    await dropDatabase(db.connection);
+    await seedDatabase(KPI, kpis);
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
+};
+
+const dropDatabase = async (mongooseConnection: mongoose.Connection) => {
+  await mongooseConnection.db.dropDatabase();
+};
+
+const seedDatabase = async (model: any, data: any) => {
+  model.insertMany(data);
+  console.log("Database seeded");
 };
 
 export default connectDatabase;
